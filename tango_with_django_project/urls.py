eedf4e79baebf60f942ea6dc32d54cab3b436e21
KYDinNUOL <you@example.com>
2017-12-15 11:25:42 +0700

    End of Ch.04 at NUOL

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls import url
 from django.conf.urls import include 
 from django.urls import path
 from rango import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')), 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
