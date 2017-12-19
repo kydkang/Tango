f73efaeba000a2edc368ba7bcb162c6cbfc95ad5
KYDinNUOL <you@example.com>
2017-12-19 11:32:52 +0700

    middle

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify 
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -21,3 +22,10 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to = 'profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
