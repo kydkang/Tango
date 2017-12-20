bd154b632e19729ada2f74a35d85f34ffd6b913f
KYD <kydkang@gmail.com>
2017-12-21 05:30:33 +0700

    home

--- rango/views.py
+++ rango/views.py
@@ -8,18 +8,23 @@ from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime 
 
 
 def index(request):
+    request.session.set_test_cookie() 
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
     # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
-
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+    return response 
 
 def about(request):
+    print("TEST COOKIE WORKED!!!") 
+    request.session.delete_test_cookie() 
     #   return HttpResponse("This is about page  <br/> <a href='/rango/'>Index</a>")
     context_dict = {'about_string': "This is about page.", 'my_name': "KYD"}
     return render(request, 'rango/about.html', context=context_dict)
@@ -122,3 +127,20 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index')) 
+
+
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+ 
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        visits = 1
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
