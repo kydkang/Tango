ca4e082bfc99c8e63d3834eefdc8c3d4f72dddfb
KYDinNUOL <you@example.com>
2017-12-22 14:43:29 +0700

    After chapter 16

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'bootstrap_toolkit', 
 ]
 REGISTRATION_OPEN = True
 ACCOUNT_ACTIVATION_DAYS = 7
