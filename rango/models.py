76140ca77c8caa48d10410d6f668cd0e4a62b6b0
KYD <kydkang@gmail.com>
2017-12-25 05:19:50 +0700

    Deploy1

--- rango/models.py
+++ rango/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.template.defaultfilters import slugify 
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+def validate_number(value):
+    if value < 0  :  # Your conditions here
+        return 0
+ 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0 )
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
     def save(self, *args, **kwargs): 
+        if self.views < 0:
+            self.views = 0
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
     class Meta:
